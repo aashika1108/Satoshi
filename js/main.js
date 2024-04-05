06dab95894062495bddaa8e67ae54958f3edc16a
aashika1108 <ashika.vachhani1234@gmail.com>
2024-04-04 23:44:37 -0400

    menu page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var menuToggle = document.querySelector('.menu-toggle');
-    var menuLinks = document.querySelector('.menu-links');
+document.addEventListener("DOMContentLoaded", function () {
+  var menuToggle = document.querySelector(".menu-toggle");
+  var menuLinks = document.querySelector(".menu-links");
 
-    menuToggle.addEventListener('click', function() {
-        menuLinks.classList.toggle('show'); // Toggle class to show/hide menu
+  menuToggle.addEventListener("click", function () {
+    menuLinks.classList.toggle("show"); // Toggle class to show/hide menu
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const searchInput = document.getElementById("searchInput");
+  const menuItems = document.querySelectorAll(".recipe");
+
+  searchInput.addEventListener("input", function () {
+    const searchText = searchInput.value.toLowerCase();
+    menuItems.forEach(function (menuItem) {
+      const title = menuItem.querySelector("h3").textContent.toLowerCase();
+      if (title.includes(searchText)) {
+        menuItem.style.display = "flex";
+      } else {
+        menuItem.style.display = "none";
+      }
     });
+  });
 });
