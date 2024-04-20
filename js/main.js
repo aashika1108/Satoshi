3b819a7d9e82e93d2ce47da8bf246bd25818777d
1106amisha <amishanaresh12@gmail.com>
2024-04-19 22:57:43 -0400

    change

--- js/main.js
+++ js/main.js
@@ -10,29 +10,26 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  if (window.location.pathname.includes("menu.html")) {
-    const searchInput = document.getElementById("searchInput");
-    if (searchInput) {
+  const searchInput = document.getElementById("searchInput");
+  if (searchInput) {
       const menuItems = document.querySelectorAll(".recipe");
-      const boxMenus = document.querySelectorAll(".box-menu");
 
       searchInput.addEventListener("input", function () {
-        const searchText = searchInput.value.toLowerCase();
-        menuItems.forEach(function (menuItem) {
-          const title = menuItem.querySelector("h3").textContent.toLowerCase();
-          if (title.includes(searchText)) {
-            menuItem.style.display = "flex";
-          } else {
-            menuItem.style.display = "none";
-          }
-        });
-        boxMenus.forEach(function (boxMenu) {
-          boxMenu.style.display = "none";
-        });
+          const searchText = searchInput.value.toLowerCase();
+          menuItems.forEach(function (menuItem) {
+              const titleElement = menuItem.querySelector("h3");
+              if (titleElement) {
+                  const title = titleElement.textContent.toLowerCase();
+                  if (title.includes(searchText)) {
+                      menuItem.style.display = "flex";
+                  } else {
+                      menuItem.style.display = "none";
+                  }
+              }
+          });
       });
-    } else {
+  } else {
       console.error("Search input element not found.");
-    }
   }
 });
 
